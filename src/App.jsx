5f9c5c0176064da644529b1c18346a9ca6d3bf46
Marina Lendt <marinalendt@gmail.com>
2026-01-15 15:59:18 +0100

    adding UseEffect to remove a margin om body, eventhough I got margin 0 in GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { TodoInput } from "./components/TodoInput";
+import { useEffect } from "react";
 
 export const App = () => {
+  useEffect(() => { document.body.style.margin = '0'; }, []); //This is for removing the margin top in the body, eventhough I have margin 0 in GlobalStyle. 
+
   return (
     <>
       <TodoInput />
