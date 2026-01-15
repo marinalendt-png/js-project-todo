ded83db673f3a3eb59fae1d2ba94ba3b90b15a64
Marina Lendt <marinalendt@gmail.com>
2026-01-15 16:00:35 +0100

    adding function to todoStore

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,10 +1,39 @@
 import { create } from "zustand";
+import { palette } from "../styles/colors";
 
-export const useTodoStore = create((set) => ({
+export const useTodoStore = create((set, get) => ({
   todos: [],
-  addTodo: (text) => set((state) => ({ todos: [...state.todos, { text, completed: false, id: Date.now() }] })),
-  removeTodo: (id) => set((state) => ({ todos: state.todos.filter(todo => todo.id !== id) })),
-  toggleTodo: (id) => set((state) => ({
-    todos: state.todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
-  }))
+  colorIndex: 0,
+
+  addTodo: (text) => {
+
+    const state = get(); //hämta nuvarande state
+    const color = palette[state.colorIndex]; //välj färg enligt palette (index)
+
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+      color, //bakgrundsfärg
+    };
+
+    //uppdaterar Todos och färgIndex
+    set({
+      todos: [...state.todos, newTodo],
+      colorIndex: (state.colorIndex + 1) % palette.length,
+    });
+  },
+
+  removeTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter(todo => todo.id !== id)
+    })),
+
+  //Markera todo som klar/toggle
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    })),
 }));
